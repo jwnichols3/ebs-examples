6335f4450de631013a4d16ac3dbbc8909b4afbda
Rocket (aka John Nichols) <jwnichols3@gmail.com>
2023-08-21 14:59:56 -0700

    Create CW Alert for ImpairedVol

--- terraform-alert-automation/ebs-lambda-cw-alarm-impairedvol-create.py
+++ terraform-alert-automation/ebs-lambda-cw-alarm-impairedvol-create.py
@@ -8,45 +8,45 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    # Log the incoming event
-    logger.info(f"Received event: {event}")
+    try:
+        # Extract the EBS volume ID from the event object
+        volume_id = event["detail"]["responseElements"]["volumeId"]
 
-    # Extract the EBS volume ID from the event object
-    volume_id = event["detail"]["responseElements"]["volumeId"]
+        # SNS topic ARN from environment variables
+        sns_topic_arn = os.environ["SNS_TOPIC_ARN"]
 
-    # SNS topic ARN from environment variables
-    sns_topic_arn = os.environ["SNS_TOPIC_ARN"]
+        # Boto3 client for CloudWatch
+        cloudwatch_client = boto3.client("cloudwatch")
 
-    # Boto3 client for CloudWatch
-    cloudwatch_client = boto3.client("cloudwatch")
+        # Create or update CloudWatch alarm for the EBS volume
+        response = cloudwatch_client.put_metric_alarm(
+            AlarmName="ImpairedVol_" + volume_id,
+            ComparisonOperator="GreaterThanOrEqualToThreshold",
+            EvaluationPeriods=1,
+            MetricName="VolumeIdleTime",
+            Namespace="AWS/EBS",
+            Period=300,
+            Statistic="Average",
+            Threshold=0.01,
+            ActionsEnabled=True,
+            AlarmActions=[sns_topic_arn],
+            AlarmDescription="Alarm for EBS volume " + volume_id,
+            Dimensions=[
+                {"Name": "VolumeId", "Value": volume_id},
+            ],
+            Unit="Seconds",
+        )
 
-    # Create or update CloudWatch alarm for the EBS volume
-    response = cloudwatch_client.put_metric_alarm(
-        AlarmName="ImpairedVol_" + volume_id,
-        ComparisonOperator="GreaterThanOrEqualToThreshold",
-        EvaluationPeriods=1,
-        MetricName="VolumeIdleTime",
-        Namespace="AWS/EBS",
-        Period=300,
-        Statistic="Average",
-        Threshold=0.01,
-        ActionsEnabled=True,
-        AlarmActions=[sns_topic_arn],
-        AlarmDescription="Alarm for EBS volume " + volume_id,
-        Dimensions=[
-            {"Name": "VolumeId", "Value": volume_id},
-        ],
-        Unit="Seconds",
-    )
+        # Log the response from the put_metric_alarm call
+        logger.info(f"CloudWatch alarm response: {response}")
 
-    # Log the response from the put_metric_alarm call
-    logger.info(f"CloudWatch alarm response: {response}")
+        return {"statusCode": 200, "body": response}
 
-    human_readable_status = (
-        "Successfully created or updated CloudWatch alarm"
-        if response
-        else "Failed to create or update CloudWatch alarm"
-    )
-    logger.info(human_readable_status)
-
-    return {"statusCode": 200, "body": human_readable_status}
+    except Exception as e:
+        # Log the error and return a 500 error response
+        logger.info(f"Received event: {event}")
+        logger.error(f"An error occurred: {e}")
+        return {
+            "statusCode": 500,
+            "body": "An error occurred while processing the event. See CloudWatch logs for details.",
+        }
--- terraform-alert-automation/main.tf
+++ terraform-alert-automation/main.tf
@@ -3,15 +3,9 @@ provider "aws" {
   profile = "hme"
 }
 
-# Create SNS Topic
-resource "aws_sns_topic" "ebs_alarms" {
-  name = "ebs_alarms"
-}
-
-# IAM Role for Lambda Function
+# IAM role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -26,11 +20,10 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# IAM Policy for Lambda Function
+# IAM policy for Lambda
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.lambda_role.id
-
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -41,6 +34,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents",
           "cloudwatch:PutMetricAlarm",
+          "sns:Publish"
         ],
         Resource = "*"
       }
@@ -48,14 +42,25 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
-# Lambda Function
+# SNS topic
+resource "aws_sns_topic" "ebs_alarms" {
+  name = "ebs_alarms"
+}
+
+# SNS topic subscription
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.ebs_alarms.arn
+  protocol  = "email"
+  endpoint  = "example@example.com"
+}
+
+# Lambda function
 resource "aws_lambda_function" "ebs_alarm_lambda" {
-  filename         = "ebs-lambda-cw-alarm-impairedvol-create.zip"
-  function_name    = "ebs_alarm_lambda"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "ebs-lambda-cw-alarm-impairedvol-create.lambda_handler"
-  source_code_hash = filebase64sha256("ebs-lambda-cw-alarm-impairedvol-create.zip")
-  runtime          = "python3.8"
+  filename      = "ebs-lambda-cw-alarm-impairedvol-create.zip"
+  function_name = "ebs_alarm_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "ebs-lambda-cw-alarm-impairedvol-create.lambda_handler"
+  runtime       = "python3.8"
 
   environment {
     variables = {
@@ -64,39 +69,34 @@ resource "aws_lambda_function" "ebs_alarm_lambda" {
   }
 }
 
-# EventBridge Rule for EBS Volume Creation
+# CloudWatch event rule
 resource "aws_cloudwatch_event_rule" "ebs_creation_rule" {
-  name        = "ebs-volume-creation"
-  description = "EBS Volume Creation"
+  name        = "ebs-volume-alert-creation"
+  description = "Trigger when an EBS volume is created"
 
-  event_pattern = jsonencode({
-    "source" : ["aws.ec2"],
-    "detail-type" : ["AWS API Call via CloudTrail"],
-    "detail" : {
-      "eventName" : ["CreateVolume"]
-    }
-  })
+  event_pattern = <<PATTERN
+{
+  "source": ["aws.ec2"],
+  "detail-type": ["AWS API Call via CloudTrail"],
+  "detail": {
+    "eventName": ["CreateVolume"]
+  }
+}
+PATTERN
 }
 
-# Event Target to Invoke Lambda
+# CloudWatch event target
 resource "aws_cloudwatch_event_target" "ebs_creation_target" {
   rule      = aws_cloudwatch_event_rule.ebs_creation_rule.name
   target_id = "EBSVolumeCreation"
   arn       = aws_lambda_function.ebs_alarm_lambda.arn
 }
 
-# Lambda Permission to Allow EventBridge to Invoke Function
+# Permission for CloudWatch to invoke Lambda
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ebs_alarm_lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.ebs_creation_rule.arn
-}
-
-# SNS Topic Subscription (Optional: Add subscriptions as needed)
-resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.ebs_alarms.arn
-  protocol  = "email"
-  endpoint  = "jnicamzn+sns-ebs@amazon.com"
 }
