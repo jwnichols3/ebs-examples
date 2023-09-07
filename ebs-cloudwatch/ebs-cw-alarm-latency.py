4c36fb361ff6fd20a6e3be6d44e9e4608a8f8c3a
Rocket (aka John Nichols) <jwnichols3@gmail.com>
2023-09-07 08:43:37 -0700

    Refactor Checkpoint - behavior matrix

--- ebs-cloudwatch/ebs-cw-alarm-latency.py
+++ ebs-cloudwatch/ebs-cw-alarm-latency.py
@@ -3,8 +3,12 @@ import argparse
 import sys
 import logging
 
+# Constants
 PAGINATOR_COUNT = 100
 SNS_ALARM_ACTION_ARN = "arn:aws:sns:us-west-2:338557412966:ebs_alarms"
+DEFAULT_THRESHOLD = 200
+EVALUATION_PERIODS = 1
+DATAPOINTS_TO_ALARM = 1
 
 # Initialize logging
 logging.basicConfig(
@@ -12,24 +16,16 @@ logging.basicConfig(
 )
 
 
-def main(tag=None, refresh=False, tagless=False):
-    """
-    The main function for creating or updating CloudWatch Alarms for EBS Read Latency.
-
-    Parameters:
-        tag (str): Optional tag to filter volumes.
-        refresh (bool): If True, existing alarms will be updated.
-        tagless (bool): If True, includes volumes without tags when updating.
-    """
-
+def main(tag=None, refresh=False, tagless=False, rename=False):
+    """Main function that handles creating and updating CloudWatch Alarms for EBS volumes."""
     ec2, cloudwatch, sns = init_aws_clients()
-    skipped_tagless_volumes = []
     updated_alarms = []
 
     if not check_sns_exists(sns):
         logging.error("SNS topic doesn't exist or is not accessible.")
         return
 
+    # Get existing alarm names
     alarm_names = [
         alarm["AlarmName"]
         for page in cloudwatch.get_paginator("describe_alarms").paginate(
@@ -38,6 +34,7 @@ def main(tag=None, refresh=False, tagless=False):
         for alarm in page["MetricAlarms"]
     ]
 
+    # Get existing volume IDs
     volume_ids = [
         vol["VolumeId"]
         for page in ec2.get_paginator("describe_volumes").paginate(
@@ -50,172 +47,147 @@ def main(tag=None, refresh=False, tagless=False):
         tagname = ""
         volume = ec2.describe_volumes(VolumeIds=[volume_id])
         tags = volume["Volumes"][0].get("Tags", [])
+
         for t in tags:
             if t["Key"] == tag:
                 tagname = t["Value"]
                 break
 
-        if refresh and tag and (tagname or (not tagname and tagless)):
-            create_read_latency_alarm(volume_id, tagname, cloudwatch, refresh=True)
-            updated_alarms.append(volume_id)
-        elif tag and tagname:
-            alarm_name = (
-                f"Read Latency {tagname} {volume_id}"  # construct the alarm name
-            )
-            if alarm_name not in alarm_names:  # Check if the alarm already exists
-                create_read_latency_alarm(volume_id, tagname, cloudwatch)
-            else:
-                logging.info(
-                    f"Skipping existing alarm '{alarm_name}' for volume {volume_id}"
-                )
-        elif not tag:
-            alarm_name = f"Read Latency {volume_id}"  # construct the alarm name
-            if refresh:  # If refresh flag is set, update the alarm
-                create_read_latency_alarm(volume_id, tagname, cloudwatch, refresh=True)
-            elif alarm_name not in alarm_names:  # Check if the alarm already exists
-                create_read_latency_alarm(volume_id, tagname, cloudwatch)
-            else:
-                logging.info(
-                    f"Skipping existing alarm '{alarm_name}' for volume {volume_id}"
-                )
+        alarm_name = construct_alarm_name("Read Latency", tagname, volume_id)
 
-    if skipped_tagless_volumes:
-        logging.info(
-            f"Skipped volumes without tag: {', '.join(skipped_tagless_volumes)}"
+        # Check if an alarm with a similar pattern already exists
+        existing_alarm = any(
+            name.startswith(f"Read Latency {volume_id}") for name in alarm_names
         )
+
+        if existing_alarm and not rename:
+            logging.info(
+                f"Alert for volume {volume_id} already exists. Use the --rename option to create a new alarm and remove the existing one."
+            )
+            continue
+
+        if rename and existing_alarm:
+            # Remove the existing alarm
+            cloudwatch.delete_alarms(AlarmNames=[alarm_name])
+
+        create_latency_alarm(
+            volume_id,
+            tagname,
+            cloudwatch,
+            metric_names=["VolumeTotalReadTime", "VolumeReadOps"],
+            threshold=DEFAULT_THRESHOLD,
+            evaluation_periods=EVALUATION_PERIODS,
+            datapoints_to_alarm=DATAPOINTS_TO_ALARM,
+        )
+        updated_alarms.append(volume_id)
 
     if updated_alarms:
         logging.info(f"Updated alarms for volumes: {', '.join(updated_alarms)}")
 
 
-def init_aws_clients():
-    """
-    Initialize AWS service clients.
+def construct_alarm_name(base_name, tagname, volume_id):
+    """Constructs the alarm name based on base name, tag, and volume ID."""
+    return (
+        f"{base_name} {tagname} {volume_id}" if tagname else f"{base_name} {volume_id}"
+    )
 
-    Returns:
-        ec2, cloudwatch, sns: boto3 client objects for EC2, CloudWatch, and SNS.
-    """
+
+def init_aws_clients():
+    """Initializes and returns AWS service clients."""
     try:
         ec2 = boto3.client("ec2")
         cloudwatch = boto3.client("cloudwatch")
         sns = boto3.client("sns")
         return ec2, cloudwatch, sns
     except Exception as e:
-        logging.error(f"Error initializing clients: {e}")
+        logging.error(f"Error initializing AWS clients: {e}")
         sys.exit(1)
 
 
 def check_sns_exists(sns):
-    """
-    Check if the SNS topic exists.
-
-    Parameters:
-        sns (boto3.SNS.Client): The boto3 SNS client object.
-
-    Returns:
-        bool: True if the SNS topic exists, False otherwise.
-    """
+    """Checks if the SNS topic exists."""
     try:
         sns.get_topic_attributes(TopicArn=SNS_ALARM_ACTION_ARN)
         return True
     except Exception as e:
-        logging.error(f"Error checking SNS: {e}")
-        sys.exit(1)
+        logging.error(f"Error checking SNS topic: {e}")
+        return False
 
 
-def create_read_latency_alarm(volume_id, tagname, cloudwatch, refresh=False):
-    """
-    Create or update a CloudWatch Alarm for EBS Read Latency.
-
-    Parameters:
-        volume_id (str): The EBS volume ID.
-        tagname (str): The tag name for the alarm.
-        cloudwatch (boto3.CloudWatch.Client): The boto3 CloudWatch client object.
-        refresh (bool): If True, the existing alarm will be updated.
-    """
-    try:
-        alarm_name = (
-            f"Read Latency {tagname} {volume_id}"
-            if tagname
-            else f"Read Latency {volume_id}"
-        )
-        alarm_details = {
-            "AlarmName": alarm_name,
-            "AlarmActions": [SNS_ALARM_ACTION_ARN],
-            "EvaluationPeriods": 1,
-            "DatapointsToAlarm": 1,
-            "Threshold": 200,
-            "ComparisonOperator": "GreaterThanOrEqualToThreshold",
-            "TreatMissingData": "missing",
-            "Metrics": [
-                {
-                    "Id": "e1",
-                    "Expression": "(m1 / m2) * 1000",
-                    "Label": "ReadLatency",
-                    "ReturnData": True,
-                },
-                {
-                    "Id": "m1",
-                    "MetricStat": {
-                        "Metric": {
-                            "Namespace": "AWS/EBS",
-                            "MetricName": "VolumeTotalReadTime",
-                            "Dimensions": [{"Name": "VolumeId", "Value": volume_id}],
-                        },
-                        "Period": 300,
-                        "Stat": "Average",
+def create_latency_alarm(
+    volume_id,
+    tagname,
+    cloudwatch,
+    metric_names,
+    threshold,
+    evaluation_periods,
+    datapoints_to_alarm,
+):
+    """Creates or updates a CloudWatch Alarm for EBS latency."""
+    alarm_name = construct_alarm_name("Read Latency", tagname, volume_id)
+    alarm_details = {
+        "AlarmName": alarm_name,
+        "AlarmActions": [SNS_ALARM_ACTION_ARN],
+        "EvaluationPeriods": evaluation_periods,
+        "DatapointsToAlarm": datapoints_to_alarm,
+        "Threshold": threshold,
+        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+        "TreatMissingData": "missing",
+        "Metrics": [
+            {
+                "Id": "e1",
+                "Expression": f"(m1 / m2) * 1000",
+                "Label": "Latency",
+                "ReturnData": True,
+            },
+            {
+                "Id": "m1",
+                "MetricStat": {
+                    "Metric": {
+                        "Namespace": "AWS/EBS",
+                        "MetricName": metric_names[0],
+                        "Dimensions": [{"Name": "VolumeId", "Value": volume_id}],
                     },
-                    "ReturnData": False,
+                    "Period": 300,
+                    "Stat": "Average",
                 },
-                {
-                    "Id": "m2",
-                    "MetricStat": {
-                        "Metric": {
-                            "Namespace": "AWS/EBS",
-                            "MetricName": "VolumeReadOps",
-                            "Dimensions": [{"Name": "VolumeId", "Value": volume_id}],
-                        },
-                        "Period": 300,
-                        "Stat": "Average",
+                "ReturnData": False,
+            },
+            {
+                "Id": "m2",
+                "MetricStat": {
+                    "Metric": {
+                        "Namespace": "AWS/EBS",
+                        "MetricName": metric_names[1],
+                        "Dimensions": [{"Name": "VolumeId", "Value": volume_id}],
                     },
-                    "ReturnData": False,
+                    "Period": 300,
+                    "Stat": "Average",
                 },
-            ],
-        }
-
-        if refresh:
-            cloudwatch.delete_alarms(AlarmNames=[alarm_name])
-
-        cloudwatch.put_metric_alarm(**alarm_details)
-        logging.info(
-            f"New or updated alarm '{alarm_name}' created for volume {volume_id}"
-        )
-    except Exception as e:
-        logging.error(f"Error creating or updating alarm: {e}")
+                "ReturnData": False,
+            },
+        ],
+    }
+    cloudwatch.put_metric_alarm(**alarm_details)
+    logging.info(f"New or updated alarm '{alarm_name}' created for volume {volume_id}")
 
 
 def parse_args():
-    """
-    Parse command line arguments.
-
-    Returns:
-        argparse.Namespace: The parsed arguments as a Namespace object.
-    """
+    """Parses and returns command line arguments."""
     parser = argparse.ArgumentParser(
-        description="Create CloudWatch Alarms for EBS Read Latency."
+        description="Create or update CloudWatch Alarms for EBS latency."
     )
-    parser.add_argument("--tag", help="Tag name to be appended to Alarm name")
+    parser.add_argument("--tag", help="Tag name to filter volumes.")
     parser.add_argument(
-        "--tagless",
-        action="store_true",
-        help="Include volumes without tags when using --refresh and --tag",
+        "--refresh", action="store_true", help="Refresh existing alarms."
     )
     parser.add_argument(
-        "--refresh", action="store_true", help="Refresh existing alarms"
+        "--tagless", action="store_true", help="Include volumes without tags."
     )
+    parser.add_argument("--rename", action="store_true", help="Rename existing alarms.")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    main(tag=args.tag, refresh=args.refresh, tagless=args.tagless)
+    main(tag=args.tag, refresh=args.refresh, tagless=args.tagless, rename=args.rename)
