c1fd321a2892607103f9f7708650909ca5197acc
Rocket (aka John Nichols) <jwnichols3@gmail.com>
2023-10-17 09:03:42 -0700

    Updating the user script to be more elegant for testing disk i/o

--- ebs-end-to-end-testing/e2e-launch-ec2-instances.py
+++ ebs-end-to-end-testing/e2e-launch-ec2-instances.py
@@ -105,15 +105,35 @@ def main():
 def get_user_data_script():
     user_data_script = """#!/bin/bash
 yum -y install fio
-for device_path in /dev/nvme*n*; do
-  # Random read/write I/O
-  fio --name=random-rw --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=4 --size=1g --time_based --filename=$device_path &
-  # Sequential read/write I/O
-  fio --name=sequential-rw --ioengine=posixaio --rw=rw --bs=128k --numjobs=4 --size=1g --time_based --filename=$device_path &
-  # Random write I/O
-  fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=4k --numjobs=4 --size=1g --time_based --filename=$device_path &
-  # Random read I/O
-  fio --name=random-read --ioengine=posixaio --rw=randread --bs=4k --numjobs=4 --size=1g --time_based --filename=$device_path &
+yum -y install parted
+
+# Get the root partition and the root device
+root_partition=$(df --output=source / | tail -1)
+root_device=$(echo $root_partition | sed -E 's/p[0-9]+$//')
+
+for device_path in /dev/nvme*n1; do  # Only match NVMe namespaces, not partitions
+  # Skip if the device_path is the root device or the root partition
+  if [[ "$device_path" != "$root_device" && "$device_path" != "$root_partition" ]]; then
+    device_name=$(basename $device_path)
+    
+    # Create a single partition on the device
+    parted $device_path --script mklabel gpt mkpart primary ext4 0% 100%
+    
+    # Wait a bit for the partition table to get re-read
+    sleep 5
+    
+    # Format the partition to ext4
+    mkfs.ext4 "${device_path}p1"
+    
+    # Create a mount point and mount the partition
+    mkdir -p "/mnt/${device_name}"
+    mount "${device_path}p1" "/mnt/${device_name}"
+    
+    # Random read/write I/O
+    fio --name=random-rw_${device_name} --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=2m --time_based --runtime=100000h --filename="/mnt/${device_name}/fio_test_file" &
+
+    fio --name=sequential-rw_${device_name} --ioengine=posixaio --rw=rw --bs=128k --numjobs=1 --size=1g --time_based --runtime=100000h --filename="/mnt/${device_name}/fio_test_file" &
+  fi
 done"""
     return user_data_script
 
--- ebs-end-to-end-testing/ebs-load-test-linux.sh
+++ ebs-end-to-end-testing/ebs-load-test-linux.sh
@@ -7,7 +7,7 @@ root_device=$(df --output=source / | tail -1)
 for device_path in /dev/nvme*n*; do
   if [ "$device_path" != "$root_device" ]; then
     # Random read/write I/O
-    fio --name=random-rw --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=1g --time_based --runtime=100000h --filename=$device_path &
+    fio --name=random-rw --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=256m --time_based --runtime=100000h --filename=$device_path &
     # Sequential read/write I/O
     fio --name=sequential-rw --ioengine=posixaio --rw=rw --bs=128k --numjobs=1 --size=1g --time_based --runtime=100000h --filename=$device_path &
 
@@ -15,15 +15,58 @@ for device_path in /dev/nvme*n*; do
 done
 
 ### Loop through all attached volumes and run 1 fio jobs per volume
+
 #!/bin/bash
 yum -y install fio
-root_device=$(df --output=source / | tail -1)
-for device_path in /dev/nvme*n*; do
-  if [ "$device_path" != "$root_device" ]; then
+
+# Get the root partition and the root device
+root_partition=$(df --output=source / | tail -1)
+root_device=$(echo $root_partition | sed -E 's/p[0-9]+$//')
+
+for device_path in /dev/nvme*n1; do  # Only match NVMe namespaces, not partitions
+  # Skip if the device_path is the root device or the root partition
+  if [[ "$device_path" != "$root_device" && "$device_path" != "$root_partition" ]]; then
+    device_name=$(basename $device_path)
     # Random read/write I/O
-    fio --name=random-rw --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=1g --time_based --runtime=100000h --filename=$device_path &
+    fio --name=random-rw_${device_name} --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=2m --time_based --runtime=100000h --filename=$device_path &
   fi
 done
+
+## Partition and format the additional EBS volumes and run random and sequencial:
+
+#!/bin/bash
+yum -y install fio
+yum -y install parted
+
+# Get the root partition and the root device
+root_partition=$(df --output=source / | tail -1)
+root_device=$(echo $root_partition | sed -E 's/p[0-9]+$//')
+
+for device_path in /dev/nvme*n1; do  # Only match NVMe namespaces, not partitions
+  # Skip if the device_path is the root device or the root partition
+  if [[ "$device_path" != "$root_device" && "$device_path" != "$root_partition" ]]; then
+    device_name=$(basename $device_path)
+    
+    # Create a single partition on the device
+    parted $device_path --script mklabel gpt mkpart primary ext4 0% 100%
+    
+    # Wait a bit for the partition table to get re-read
+    sleep 5
+    
+    # Format the partition to ext4
+    mkfs.ext4 "${device_path}p1"
+    
+    # Create a mount point and mount the partition
+    mkdir -p "/mnt/${device_name}"
+    mount "${device_path}p1" "/mnt/${device_name}"
+    
+    # Random read/write I/O
+    fio --name=random-rw_${device_name} --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=2m --time_based --runtime=100000h --filename="/mnt/${device_name}/fio_test_file" &
+
+    fio --name=sequential-rw_${device_name} --ioengine=posixaio --rw=rw --bs=128k --numjobs=1 --size=1g --time_based --runtime=100000h --filename="/mnt/${device_name}/fio_test_file" &
+  fi
+done
+
 
 ### The fio job to run while logged in
 sudo fio --name=random-rw --ioengine=posixaio --rw=randrw --rwmixread=70 --bs=4k --numjobs=1 --size=1g --time_based --runtime=100000h --filename=/dev/nvme1n1 &
