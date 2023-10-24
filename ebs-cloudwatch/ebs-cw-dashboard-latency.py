0a0133b64e00e9c5af0bb3bbc8c209010b8eeabf
Rocket (aka John Nichols) <jwnichols3@gmail.com>
2023-10-24 13:09:28 -0700

    Feature - now separates widgets for each volume instead of a single widget. TODO: sharding if the number of metrics > 2500 Update - added config constants for Widgets Update - added config constant for Region (todo: more Region-centric work, such as EBS volumes come from one region, the CW Dashboard is written to a different region).

--- ebs-cloudwatch/ebs-cw-dashboard-latency.py
+++ ebs-cloudwatch/ebs-cw-dashboard-latency.py
@@ -7,6 +7,9 @@ class Config:
     PAGINATION_COUNT = 300  # Set the number of items per page
     DASHBOARD_NAME = "EBS_Latency_Dashboard"
     DASHBOARD_PERIOD = 60
+    DASHBOARD_WIDGET_WIDTH = 6
+    DASHBOARD_WIDGET_HEIGHT = 6
+    DASHBOARD_REGION = "us-west-2"  # Default region if no region provided
 
 
 def main():
@@ -15,11 +18,19 @@ def main():
     )
     parser.add_argument("--verbose", action="store_true", help="Print debug statements")
     parser.add_argument(
+        "--region",
+        default=Config.DASHBOARD_REGION,
+        help=f"AWS Region (defaults to {Config.DASHBOARD_REGION}).",
+    )
+    parser.add_argument(
         "--dry-run",
         action="store_true",
         help="Print dashboard JSON but do not create it",
     )
     args = parser.parse_args()
+
+    if args.region:
+        Config.DASHBOARD_REGION = args.region
 
     create_dashboard(verbose=args.verbose, dry_run=args.dry_run)
 
@@ -41,100 +52,97 @@ def create_dashboard(verbose=False, dry_run=False):
     cloudwatch = boto3.client("cloudwatch")
     volumes = get_ebs_volumes()
 
-    metrics = []
+    widgets = []
+    # y_position = 0  # Initial Y-coordinate position for the widget
+
     for i, volume in enumerate(volumes):
         print(f"Constructing {volume}...")
-        metrics.extend(
+        metrics = [
             [
-                [
-                    {
-                        "expression": f"(m{i*4+2}/m{i*4+1})*1000",
-                        "label": f"{volume}_ReadLatency",
-                        "id": f"e{i*2+1}",
-                        "region": "us-west-2",
-                    }
-                ],
-                [
-                    {
-                        "expression": f"(m{i*4+4}/m{i*4+3})*1000",
-                        "label": f"{volume}_WriteLatency",
-                        "id": f"e{i*2+2}",
-                        "region": "us-west-2",
-                    }
-                ],
-                [
-                    "AWS/EBS",
-                    "VolumeReadOps",
-                    "VolumeId",
-                    volume,
-                    {
-                        "region": "us-west-2",
-                        "visible": False,
-                        "id": f"m{i*4+1}",
-                        "label": f"{volume}_VolumeReadOps",
-                    },
-                ],
-                [
-                    "AWS/EBS",
-                    "VolumeTotalReadTime",
-                    "VolumeId",
-                    volume,
-                    {
-                        "region": "us-west-2",
-                        "visible": False,
-                        "id": f"m{i*4+2}",
-                        "label": f"{volume}_VolumeTotalReadTime",
-                    },
-                ],
-                [
-                    "AWS/EBS",
-                    "VolumeWriteOps",
-                    "VolumeId",
-                    volume,
-                    {
-                        "region": "us-west-2",
-                        "visible": False,
-                        "id": f"m{i*4+3}",
-                        "label": f"{volume}_VolumeWriteOps",
-                    },
-                ],
-                [
-                    "AWS/EBS",
-                    "VolumeTotalWriteTime",
-                    "VolumeId",
-                    volume,
-                    {
-                        "region": "us-west-2",
-                        "visible": False,
-                        "id": f"m{i*4+4}",
-                        "label": f"{volume}_VolumeTotalWriteTime",
-                    },
-                ],
-            ]
-        )
-
-    dashboard_body = json.dumps(
-        {
-            "widgets": [
                 {
-                    "type": "metric",
-                    "x": 0,
-                    "y": 0,
-                    "width": 12,
-                    "height": 6,
-                    "properties": {
-                        "metrics": metrics,
-                        "view": "timeSeries",
-                        "stacked": False,
-                        "region": "us-west-2",
-                        "title": f"EBS-Latency ({volume})",
-                        "period": Config.DASHBOARD_PERIOD,
-                        "stat": "Average",
-                    },
+                    "expression": f"(m{i*4+2}/m{i*4+1})*1000",
+                    "label": f"{volume}_ReadLatency",
+                    "id": f"e{i*2+1}",
+                    "region": Config.DASHBOARD_REGION,
                 }
-            ]
+            ],
+            [
+                {
+                    "expression": f"(m{i*4+4}/m{i*4+3})*1000",
+                    "label": f"{volume}_WriteLatency",
+                    "id": f"e{i*2+2}",
+                    "region": Config.DASHBOARD_REGION,
+                }
+            ],
+            [
+                "AWS/EBS",
+                "VolumeReadOps",
+                "VolumeId",
+                volume,
+                {
+                    "region": Config.DASHBOARD_REGION,
+                    "visible": False,
+                    "id": f"m{i*4+1}",
+                    "label": f"{volume}_VolumeReadOps",
+                },
+            ],
+            [
+                "AWS/EBS",
+                "VolumeTotalReadTime",
+                "VolumeId",
+                volume,
+                {
+                    "region": Config.DASHBOARD_REGION,
+                    "visible": False,
+                    "id": f"m{i*4+2}",
+                    "label": f"{volume}_VolumeTotalReadTime",
+                },
+            ],
+            [
+                "AWS/EBS",
+                "VolumeWriteOps",
+                "VolumeId",
+                volume,
+                {
+                    "region": Config.DASHBOARD_REGION,
+                    "visible": False,
+                    "id": f"m{i*4+3}",
+                    "label": f"{volume}_VolumeWriteOps",
+                },
+            ],
+            [
+                "AWS/EBS",
+                "VolumeTotalWriteTime",
+                "VolumeId",
+                volume,
+                {
+                    "region": Config.DASHBOARD_REGION,
+                    "visible": False,
+                    "id": f"m{i*4+4}",
+                    "label": f"{volume}_VolumeTotalWriteTime",
+                },
+            ],
+        ]
+
+        widget = {
+            "type": "metric",
+            "width": Config.DASHBOARD_WIDGET_WIDTH,
+            "height": Config.DASHBOARD_WIDGET_HEIGHT,
+            "properties": {
+                "metrics": metrics,
+                "view": "timeSeries",
+                "stacked": False,
+                "region": Config.DASHBOARD_REGION,
+                "title": f"EBS-Latency ({volume})",
+                "period": Config.DASHBOARD_PERIOD,
+                "stat": "Average",
+            },
         }
-    )
+        widgets.append(widget)
+
+        # y_position += 6  # Update the Y-coordinate position for the next widget
+
+    dashboard_body = json.dumps({"widgets": widgets})
 
     if verbose or dry_run:
         print("Dashboard JSON:")
