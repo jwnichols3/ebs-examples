e570421c82748a1e50bd75ddf9c4a8156a9e66b3
Rocket (aka John Nichols) <jwnichols3@gmail.com>
2023-11-02 15:36:17 -0700

    Adding VPC endpoint lookup logic

--- vpc-endpoints-check.py
+++ vpc-endpoints-check.py
@@ -3,26 +3,32 @@ import logging
 
 
 class Config:
-    EC2_ENDPOINT_URL = "https://ec2.us-west-2.api.aws"
-    CLOUDWATCH_ENDPOINT_URL = "https://monitoring.us-west-2.amazonaws.com"
-    SNS_ENDPOINT_URL = "https://sns.us-west-2.amazonaws.com"
-    REGION = "us-west-2"
+    DEFAULT_REGION = "us-west-2"
+    EC2_ENDPOINT_URL = f"https://ec2.{DEFAULT_REGION}.api.aws"  # this is the dual-stack endpoint (IPv4 and IPv6)
+    CLOUDWATCH_ENDPOINT_URL = f"https://monitoring.{DEFAULT_REGION}.amazonaws.com"
+    SNS_ENDPOINT_URL = f"https://sns.{DEFAULT_REGION}.amazonaws.com"
 
 
 def initialize_aws_clients():
     try:
         ec2 = boto3.client(
-            "ec2", region_name=Config.REGION, endpoint_url=Config.EC2_ENDPOINT_URL
+            "ec2",
+            region_name=Config.DEFAULT_REGION,
+            endpoint_url=Config.EC2_ENDPOINT_URL,
         )
         cloudwatch = boto3.client(
             "cloudwatch",
-            region_name=Config.REGION,
+            region_name=Config.DEFAULT_REGION,
             endpoint_url=Config.CLOUDWATCH_ENDPOINT_URL,
         )
         sns = boto3.client(
-            "sns", region_name=Config.REGION, endpoint_url=Config.SNS_ENDPOINT_URL
+            "sns",
+            region_name=Config.DEFAULT_REGION,
+            endpoint_url=Config.SNS_ENDPOINT_URL,
         )
-        logging.info("Initialized AWS Client in region {}".format(Config.REGION))
+        logging.info(
+            "Initialized AWS Client in region {}".format(Config.DEFAULT_REGION)
+        )
     except Exception as e:
         logging.error("Failed to initialize AWS clients: {}".format(e))
         exit(1)
@@ -63,12 +69,43 @@ def test_sns_access(sns):
         logging.error("Failed SNS access test: {}".format(e))
 
 
+def can_connect_to_endpoint(endpoint_url):
+    """Check if the script can connect to the given endpoint URL."""
+    import requests
+
+    try:
+        response = requests.get(endpoint_url, timeout=5)
+        print(f"request response: \n{response}")
+        return True if response.status_code == 200 else False
+    except requests.RequestException:
+        return False
+
+
+def get_and_print_vpc_endpoints_dns_entries(ec2):
+    if not can_connect_to_endpoint(Config.EC2_ENDPOINT_URL):
+        logging.warning(
+            "Cannot connect to EC2 VPC Endpoint. Ensure you're running this within the VPC or have necessary connectivity setup."
+        )
+        return
+
+    try:
+        vpc_endpoints = ec2.describe_vpc_endpoints()
+        for endpoint in vpc_endpoints.get("VpcEndpoints", []):
+            print(f"VPC Endpoint ID: {endpoint['VpcEndpointId']}")
+            for entry in endpoint.get("DnsEntries", []):
+                print(f"\tDNS name: {entry['DnsName']}")
+                print(f"\tDNS IP: {entry['DnsIpAddresses']}")
+    except Exception as e:
+        logging.error("Failed to get VPC Endpoints DNS entries: {}".format(e))
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
     ec2, cloudwatch, sns = initialize_aws_clients()
     test_ec2_access(ec2)
     test_cloudwatch_access(cloudwatch)
     test_sns_access(sns)
+    get_and_print_vpc_endpoints_dns_entries(ec2)
 
 
 if __name__ == "__main__":
